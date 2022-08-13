2651f794ac3e867d3967fdf8a076f6d9507c7495
samuel.heilbroner <samuel.heilbroner@tempus.com>
2022-08-12 18:33:42 -0700

    make puzzle easier

--- lock_breaker/validation.py
+++ lock_breaker/validation.py
@@ -3,5 +3,5 @@ from lock_breaker.time import within_time_limit
 
 
 def input_is_valid(copied_text, text, current_time):
-    return levenshtein_distance(copied_text, text) < 20 and \
+    return levenshtein_distance(copied_text, text) < len(text)*0.1 and \
            within_time_limit(current_time)
