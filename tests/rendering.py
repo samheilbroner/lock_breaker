6ecad9820d12e9dc294c050ab761846411401d82
samuel.heilbroner <samuel.heilbroner@tempus.com>
2023-09-23 16:47:58 -0600

    delete tests

--- tests/rendering.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from lock_breaker.rendering import image_from_text
-
-
-def test_image_from_text():
-    image_from_text('test_text\n'*50, 'images/test.png')
-    assert os.path.exists('images/test.png')
